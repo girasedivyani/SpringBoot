ac6c68e7a0c3c73df3226c8f0d44649c81fa0870
girasedivyani <102512687+girasedivyani@users.noreply.github.com>
2023-03-07 17:29:19 +0530

    Update Logic.java

--- Logic.java
+++ Logic.java
@@ -3,15 +3,15 @@ package com.firstcalculator.logic;
 public class Logic {
 	
 		
-		public static int addNo(int a1, int a2)
+		public static int addNo(int a1, int a2)//create method for two number addition 
 		{
 			return a1+a2;
 		}
-		public static int subNo(int a1,int a2)
+		public static int subNo(int a1,int a2)//create method for two number subtraction
 		{
 			return a1-a2;
 		}
-		public static int mulNo(int a1,int a2)
+		public static int mulNo(int a1,int a2)//create method for two number multiplication
 		{
 			return a1*a2;
 		}
